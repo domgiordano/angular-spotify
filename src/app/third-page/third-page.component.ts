676ae2b7ff2c247b13bc050360a88af3a4aba0fb
domjgiordano <dominick.giordano@payzer.com>
2023-07-11 17:53:58 -0400

    Added style and pages routing - have login but does nothing

---
+++ src/app/third-page/third-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third-page',
+  templateUrl: './third-page.component.html',
+  styleUrls: ['./third-page.component.css']
+})
+export class ThirdPageComponent {
+
+}
