f20193bed98d0d5ecf384a2b461d5c69a193993a
domjgiordano <dominick.giordano@payzer.com>
2024-10-08 21:27:00 -0400

    added guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,17 +11,18 @@ import { AppComponent } from './app.component';
 import { CallbackComponent } from './components/callback/callback.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ArtistProfileComponent } from './pages/artist-profile/artist-profile.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'my-profile', component: MyProfileComponent },
-  { path: 'top-songs', component: TopSongsComponent },
-  { path: 'top-artists', component: TopArtistsComponent },
-  { path: 'top-genres', component: TopGenresComponent },
-  { path: 'playlist-generator', component: PlaylistGeneratorComponent },
-  { path: 'wrapped', component: WrappedComponent },
+  { path: 'my-profile', component: MyProfileComponent, canActivate: [AuthGuard] },
+  { path: 'top-songs', component: TopSongsComponent, canActivate: [AuthGuard]},
+  { path: 'top-artists', component: TopArtistsComponent, canActivate: [AuthGuard]},
+  { path: 'top-genres', component: TopGenresComponent, canActivate: [AuthGuard]},
+  { path: 'playlist-generator', component: PlaylistGeneratorComponent, canActivate: [AuthGuard]},
+  { path: 'wrapped', component: WrappedComponent, canActivate: [AuthGuard]},
   { path: 'callback', component: CallbackComponent },
-  { path: 'artist', component: ArtistProfileComponent },
+  { path: 'artist', component: ArtistProfileComponent, canActivate: [AuthGuard]},
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to /home
   { path: '**', redirectTo: '/home' } // Redirect all other paths to /home
 ];
